59e8e04faf277344cb3a51a5a1d6898e30b01ba8
F1r3w4all <ulissesmagalhaes@edu.unifor.br>
2025-06-08 03:18:17 -0300

    teste forcando main

--- src/main/java/com/example/nutriapp/NutriappApplication.java
+++ src/main/java/com/example/nutriapp/NutriappApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NutriappApplication {  //to cansado ja ronaldo
+public class NutriappApplication {  //to cansado ja macho
     public static void main(String[] args) {
         SpringApplication.run(NutriappApplication.class, args);
+
+        
     }
 }
