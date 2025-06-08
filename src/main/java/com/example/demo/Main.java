6bb1df72939390af43fac593b70e45e1a7944327
F1r3w4all <ulissesmagalhaes@edu.unifor.br>
2025-06-08 05:08:03 -0300

    apaguei um monte

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Main {  //to cansado ja macho
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-
-        
-    }
+   public static void main(String[] args) {
+    
+    SpringApplication.run(Main.class, args);
 }
+}
+
+
+
