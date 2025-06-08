f08776f14ad380d927bf4f4cefb56caf10643aab
F1r3w4all <ulissesmagalhaes@edu.unifor.br>
2025-06-08 19:09:07 -0300

    refactor: add @NonNull annotation to CorsRegistry parameter in addCorsMappings method

--- src/main/java/com/example/nutriapp/config/CorsConfig.java
+++ src/main/java/com/example/nutriapp/config/CorsConfig.java
@@ -3,11 +3,12 @@ package com.example.nutriapp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @SuppressWarnings("null")
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS para todas as rotas
                 .allowedOrigins("*") // Permite todas as origens
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite os métodos HTTP
