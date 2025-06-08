e0d659222c73aad741ca9c0f312b66490be19121
F1r3w4all <ulissesmagalhaes@edu.unifor.br>
2025-06-08 02:52:31 -0300

    agora vai dar

--- src/main/java/com/example/nutriapp/NutriappApplication.java
+++ src/main/java/com/example/nutriapp/NutriappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NutriappApplication {  // Nome correto da classe
+public class NutriappApplication {  //agora eu vou ver
     public static void main(String[] args) {
         SpringApplication.run(NutriappApplication.class, args);
     }
